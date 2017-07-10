d729c723862fe7fb03a25d70f139e3e43c920818
tinker <thom@hometreelab.com>
2017-07-11 09:33:03 +1000

    Initial commit

---
+++ CanterlotAvenue/CAWebPage.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace CanterlotAvenue
+{
+    public class CAWebPage : ContentPage
+    {
+        public CAWebPage()
+        {
+            Label header = new Label
+            {
+                Text = "Canterlot Avenue",
+                FontSize = 50,
+                FontAttributes = FontAttributes.Bold,
+                HorizontalOptions = LayoutOptions.Center
+            };
+
+            WebView webView = new WebView
+            {
+                Source = new UrlWebViewSource
+                {
+                    Url = "https://development.canterlotavenue.com/login/",
+                },
+                VerticalOptions = LayoutOptions.FillAndExpand
+            };
+
+            // Build the page.
+            this.Content = new StackLayout
+            {
+                Children =
+                {
+                    header,
+                    webView
+                }
+            };
+        }
+    }
+}
